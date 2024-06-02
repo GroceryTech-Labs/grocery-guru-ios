293b19b6cf1a5d82321716147c35ebc4f247bdf3
Max Maetze <max.maetze@develappers.de>
2024-06-02 13:45:57 +0200

    Added more documentation

--- Grocery Guru/Assets/Constants.swift
+++ Grocery Guru/Assets/Constants.swift
@@ -2,9 +2,16 @@ import Foundation
 
 enum Constants {
     enum Padding {
+        /// Size of 4px
         static let X: CGFloat = 4.0
+
+        /// Size of 8px
         static let S: CGFloat = 8.0
+
+        /// Size of 12px
         static let M: CGFloat = 12.0
+
+        /// Size of 16px
         static let L: CGFloat = 16.0
     }
 }
