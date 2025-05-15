e3fddcaef7475e129687475380248bf3f8018b52
Max Maetze <max.maetze@develappers.de>
2025-05-15 16:52:54 +0200

    Fixed: Failed Test

--- Grocery Guru/Models/InvoiceItem.swift
+++ Grocery Guru/Models/InvoiceItem.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class InvoiceItem {
+final class InvoiceItem: Sendable {
     var name: String
     var amount: Int
     var category: InvoiceItemCategory
